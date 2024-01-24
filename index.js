88452ca5bd8b313ec0cce10fce57f206dcd1ce1f
Vaibhav <vaibhav803@gmail.com>
2024-01-24 17:01:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req , res) => {
+    res.send('coderdotcom')
+})
+
+app.get('/login', (req , res) => {
+    res.send('<h1>Please login at backend and code</h1>')
+})
+
+app.get('/youtube',(req ,res) =>{
+    res.send("<h2>backend aur code</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
